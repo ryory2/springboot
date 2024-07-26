23e72942bf7e069937a2b0376807aa02da0d4098
ryo <hideyoshi.adm@gmail.com>
2024-07-27 04:31:22 +0900

    【概要】例外を修正 【関連処理】

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -2,50 +2,91 @@ package com.example.demo.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import com.example.demo.dto.api.CustomErrorResponse;
+import com.example.demo.utils.Utils;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public ResponseEntity<String> handleGeneralException(Exception e) {
+  public ResponseEntity<CustomErrorResponse> handleGeneralException(Exception e,
+      HttpServletRequest request) {
+    CustomErrorResponse errorResponse = Utils.createErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,
+        HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), e.getMessage(), request);
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .build();
-    // .body("An error occurred: " + e.getMessage());
+        .body(errorResponse);
   }
 
   @ExceptionHandler(UserNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
+  public ResponseEntity<CustomErrorResponse> handleUserNotFoundException(UserNotFoundException e,
+      HttpServletRequest request) {
+    CustomErrorResponse errorResponse = Utils.createErrorResponse(HttpStatus.CONFLICT,
+        HttpStatus.CONFLICT.getReasonPhrase(), e.getMessage(), request);
     return ResponseEntity.status(HttpStatus.NOT_FOUND)
-        .body(e.getMessage());
+        .body(errorResponse);
   }
 
   @ExceptionHandler(UserAlreadyExistsException.class)
   @ResponseStatus(HttpStatus.CONFLICT)
-  public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+  public ResponseEntity<CustomErrorResponse> handleUserAlreadyExistsException(
+      UserAlreadyExistsException e, HttpServletRequest request) {
+    CustomErrorResponse errorResponse = Utils.createErrorResponse(HttpStatus.CONFLICT,
+        HttpStatus.CONFLICT.getReasonPhrase(), e.getMessage(), request);
     return ResponseEntity.status(HttpStatus.CONFLICT)
-        .body(e.getMessage());
+        .body(errorResponse);
   }
 
+  @ExceptionHandler(BadCredentialsException.class)
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  @ResponseBody
+  public ResponseEntity<CustomErrorResponse> handleBadCredentialsException(
+      BadCredentialsException e, HttpServletRequest request) {
+    CustomErrorResponse errorResponse = Utils.createErrorResponse(HttpStatus.UNAUTHORIZED,
+        HttpStatus.UNAUTHORIZED.getReasonPhrase(), e.getMessage(), request);
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(errorResponse);
+  }
+
+  // @ExceptionHandler(MethodArgumentNotValidException.class)
+  // public ResponseEntity<Map<String, String>> handleValidationExceptions(
+  // MethodArgumentNotValidException ex) {
+  // Map<String, String> errors = new HashMap<>();
+  // ex.getBindingResult()
+  // .getFieldErrors()
+  // .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+  // return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+  // .body(errors);
+  // }
+
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<Map<String, Object>> handleValidationExceptions(
       MethodArgumentNotValidException ex) {
-    Map<String, String> errors = new HashMap<>();
-    ex.getBindingResult()
+    Map<String, Object> response = new HashMap<>();
+    response.put("status", "error");
+
+    Map<String, String> errors = ex.getBindingResult()
         .getFieldErrors()
-        .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(errors);
+        .stream()
+        .collect(Collectors.toMap(fieldError -> fieldError.getField(),
+            fieldError -> fieldError.getDefaultMessage()));
+
+    response.put("errors", errors);
+    return ResponseEntity.badRequest()
+        .body(response);
   }
 
   @ExceptionHandler(NoHandlerFoundException.class)
@@ -57,4 +98,5 @@ public class GlobalExceptionHandler {
     errorResponse.put("error", "The requested resource was not found: " + request.getRequestURI());
     return errorResponse;
   }
+
 }
